3511c29f2d40780b06d2816a53ce71e217ebde5a
Afraz33 <afraz3301@gmail.com>
2023-09-03 20:23:09 +0500

    Add Review Section

--- src/components/Dentists/Dentists.jsx
+++ src/components/Dentists/Dentists.jsx
@@ -6,6 +6,12 @@ function Dentists() {
       <div className={styles.top_bar}>
         <img src="/teeth_tick.png" alt="logo" className={styles.logo}></img>
         <h2 className={styles.heading_2}>100% Satisfaction Guarantee!</h2>
+        <p className={styles.description}>
+          We only provide high quality dentistry & materials backed by our 100%
+          satisfaction guarantee. If you're ever unsatisfied with your visit,
+          please let us know and we'll make it right.
+        </p>
+        <button className={styles.button}>Get Started</button>
       </div>
     </section>
   );
